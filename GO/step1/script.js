3e207d52db4a04ead4368279eaa5a8c5e5f7d7c9
KALININ ALEXEY <56075795+lelikone777@users.noreply.github.com>
2023-01-04 18:45:25 +0300

    step 2 - 2 else if

--- GO/step1/script.js
+++ GO/step1/script.js
@@ -1,38 +1,3 @@
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
-
-    if (a != null && b != null && a !== '' && b !== '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
-}
-
-if (personalMovieDB.count < 10) {
-    console.log('First part!');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('2nd Part!');
-} else if (personalMovieDB.count >= 30) {
-    console.log('3rd Part');
-} else {
-    console.log('error!');
-}
-
-
-console.log(personalMovieDB);
