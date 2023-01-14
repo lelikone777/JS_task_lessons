73521b3bfc21fc356b478e2934dc79478131a508
lelikone777 <2257855@gmail.com>
2023-01-14 08:01:10 +0300

    film - step 2

--- GO/Film/js/script.js
+++ GO/Film/js/script.js
@@ -24,3 +24,17 @@ const movieDB = {
     ]
 };
 
+document.querySelector('.promo__adv').remove();
+document.querySelector('.promo__genre').textContent = 'драма';
+document.querySelector('.promo__bg').style.backgroundImage = 'url("img/bg.jpg")';
+let movieList = document.querySelector('.promo__interactive-list');
+movieList.innerHTML = '';
+movieDB.movies.sort();
+movieDB.movies.forEach((film, i ) => {
+    movieList.innerHTML += ` 
+            <li class="promo__interactive-item">${i + 1}) ${film}
+                <div class="delete"></div>
+            </li>
+        `;
+});
+
