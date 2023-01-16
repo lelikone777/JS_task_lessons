2c840d1fff5e96a46a98d5585273c10316740bd1
lelikone777 <2257855@gmail.com>
2023-01-16 07:02:26 +0300

    Food - cards 1.0

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -140,7 +140,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     })
 
-    // const modalTimerId = setTimeout(openModal, 10000);
+    const modalTimerId = setTimeout(openModal, 10000);
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
