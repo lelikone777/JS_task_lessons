6f49c6e76d12368e0b63e0ac310be1afa7bd4817
lelikone777 <2257855@gmail.com>
2023-01-24 16:59:46 +0300

    Food - XMLHttpRequest and JSON on output;

--- FoodDist/js/script.js
+++ FoodDist/js/script.js
@@ -126,7 +126,7 @@ window.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = '';
     }
 
-    modalCloseBtn.addEventListener('click', closeModal)
+    modalCloseBtn.addEventListener('click', closeModal);
 
     modal.addEventListener('click', (e) => {
         if (e.target === modal) {
@@ -223,44 +223,95 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //Forms
     
+    // const forms = document.querySelectorAll('form');
+    //
+    // const message = {
+    //     loading: 'Загрузка',
+    //     success: 'Спасибо! Скоро мы с вами свяжемся',
+    //     failure: 'Что-то пошло не так...',
+    // };
+    //
+    // forms.forEach(item => {
+    //     postData(item);
+    // });
+    //
+    // function postData(form) {
+    //     form.addEventListener('submit', (e) => {
+    //        e.preventDefault();
+    //
+    //        const statusMessage = document.createElement('div');
+    //        statusMessage.classList.add('status');
+    //        statusMessage.textContent = message.loading;
+    //        form.append(statusMessage);
+    //
+    //
+    //        const request = new XMLHttpRequest();
+    //        request.open('POST', 'server.php');
+    //
+    //        request.setRequestHeader('Content-type', 'multipart/form-data');
+    //        const formData = new FormData(form);
+    //
+    //        request.send(formData);
+    //
+    //        request.addEventListener('load', () => {
+    //           if (request.status === 200) {
+    //               console.log(request.response);
+    //               statusMessage.textContent = message.success;
+    //           } else {
+    //               statusMessage.textContent = message.failure;
+    //           }
+    //        });
+    //     });
+    // }
+
     const forms = document.querySelectorAll('form');
 
     const message = {
-        loading: 'Загрузка',
-        success: 'Спасибо! Скоро мы с вами свяжемся',
-        failure: 'Что-то пошло не так...',
+        loading: 'loading...',
+        success: 'success!!!',
+        failure: 'failure'
     };
 
     forms.forEach(item => {
-        postData(item);
+       postData(item);
     });
-    
+
     function postData(form) {
         form.addEventListener('submit', (e) => {
-           e.preventDefault();
+            e.preventDefault();
 
-           const statusMessage = document.createElement('div');
-           statusMessage.classList.add('status');
-           statusMessage.textContent = message.loading;
-           form.append(statusMessage);
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
 
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
 
-           const request = new XMLHttpRequest();
-           request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
 
-           request.setRequestHeader('Content-type', 'multipart/form-data');
-           const formData = new FormData(form);
+            const object = {};
+            formData.forEach(function (value, key) {
+               object[key] = value;
+            });
 
-           request.send(formData);
+            const json = JSON.stringify(object);
 
-           request.addEventListener('load', () => {
-              if (request.status === 200) {
-                  console.log(request.response);
-                  statusMessage.textContent = message.success;
-              } else {
-                  statusMessage.textContent = message.failure;
-              }
-           });
+            request.send(json);
+
+            request.addEventListener('load', () => {
+               if (request.status === 200) {
+                   console.log(request.response);
+                   statusMessage.textContent = message.success;
+                   form.reset();
+                   setTimeout(() => {
+                       statusMessage.remove();
+                   }, 2000);
+               } else {
+                    statusMessage.textContent = message.failure;
+               }
+            });
         });
     }
 });
