1fa43dc5dcd99c4837227fe9ecf4bb63d1e992ee
lelikone777 <2257855@gmail.com>
2023-01-24 19:08:33 +0300

    Promice

--- GO/step1/script.js
+++ GO/step1/script.js
@@ -1,1 +1,52 @@
 'use strict';
+
+console.log('Запрос Данных...');
+
+const req = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        console.log('Подготовка данных...');
+
+        const product = {
+            name: 'TV',
+            price: 2000
+        };
+
+            resolve(product);
+    }, 2000);
+});
+
+req.then((product) => {
+    return  new Promise((resolve, reject) => {
+        setTimeout(() =>{
+            product.status = 'order';
+            resolve(product);
+        },2000);
+    });
+}).then(data => {
+    data.modify = true;
+    return data;
+}).then((data) => {
+    console.log(data);
+}).catch(() => {
+    console.error('Error!');
+}).finally(() => {
+    console.log('Finally!');
+});
+
+const test = time => {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(), time);
+    });
+};
+
+test(1000).then(() => console.log('1000 ms'));
+test(2000).then(() => console.log('2000 ms'));
+
+Promise.all([test(1000), test(2000)]).then(() => {
+    console.log('All');
+});
+
+Promise.race([test(1000), test(2000)]).then(() => {
+    console.log('All');
+});
+
